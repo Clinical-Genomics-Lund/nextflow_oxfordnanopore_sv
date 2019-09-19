2318a4a4acc2ba19a0272502097032e126c3b39e
pettestor <petter.storm@med.lu.se>
2019-09-19 12:52:55 +0200

    First commit

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,3 @@
 module load nextflow
-nextflow run pipeline.nf -with-singularity singularity/onte_2019-09-09.sif --fastq "/projects/fs1/petterst/onte/ERR2585112/*fastq.gz" -with-dag flowchart.png -with-timeline timeline.html -resume --output '/projects/fs1/petterst/onte/output/'
+nextflow run pipeline.nf -with-singularity singularity/onte_2019-09-09.sif --fastq "fastq/*fastq.gz" -with-dag flowchart.png -with-timeline timeline.html -resume --output './output/' --reference './reference' --executor 'local'
 
